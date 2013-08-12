793776be5e9194196c72925d2673ae729c50be5b
Hae Jeong <haejeong87@gmail.com>
2013-08-12 03:26:56 -0700

    dynamically concats search url from user input

--- ng/app/js/controllers.js
+++ ng/app/js/controllers.js
@@ -4,7 +4,10 @@
 
 angular.module('rgbSong.controllers', []).
   controller('AppController', function($scope) {
-    $scope.GS = {
-      apiKey: "86c661e999f7198c25661648e66cd9c1"
+    $scope.apiKey = "86c661e999f7198c25661648e66cd9c1";
+    $scope.query = "";
+    $scope.getUrl = function() {
+      // Refer to http://tinysong.com/api
+      return "http://tinysong.com/s/" + escape($scope.query) + "?format=json&limit=3&key=" + $scope.apiKey
     }
   })
